7ddf3cb5a3a079078501ba6e30652f4be76fce31
marioncouesnon <marion@craftsmen.io>
2017-08-11 12:43:17 +0200

    add img fading animation

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,24 @@
-//= require_tree .
+//= require jquery
+
+var fantome = function() {
+  $('img').each( function(i){
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight() * 0.15,
+        bottom_of_window = $(document).scrollTop() + $(window).height(),
+        pad = $(this).innerHeight() - $(this).height();
+    if( bottom_of_window > bottom_of_object - pad){
+      $(this).animate({'opacity':'1',
+                       'padding-top':'0'},
+                       500);
+    }
+  });
+}
+
+$(window).scroll( function(){
+  fantome()
+});
+
+$(document).ready(function() {
+  $('img').css('opacity', '0.2').css('padding-top', '10px');
+
+  fantome();
+});
