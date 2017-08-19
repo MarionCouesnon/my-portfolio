32aa76c08f4d84f3d5eb1aea67f7af824710eb48
marioncouesnon <marion@craftsmen.io>
2017-08-19 22:49:16 +0200

    add zoom effect for images in grids

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
+//= require imageLightbox
 
+// Image animation
 var fantome = function() {
   $('img').each( function(i){
     var bottom_of_object = $(this).offset().top + $(this).outerHeight() * 0.15,
@@ -17,8 +19,29 @@ $(window).scroll( function(){
   fantome()
 });
 
+var starting = function() {
+  $('body').addClass('noscroll');
+  if (!$( '#imagelightbox-overlay' ).length) $( '<div id="imagelightbox-overlay"></div>' ).appendTo( 'body' );
+  $( '#imagelightbox-overlay' ).removeClass("tic");
+}
+
+var ending = function() {
+  $('body').removeClass('noscroll');
+  $('#imagelightbox-overlay').addClass("tic");
+}
+
 $(document).ready(function() {
   $('img').css('opacity', '0.2').css('padding-top', '10px');
 
+  $(".zoomIn").imageLightbox( {
+    onStart: function() { starting() },
+    onEnd: function() { ending() }
+  })
+
   fantome();
 });
+
+// $("imagelightbox-overlay").click(function() {
+//   $('body').removeClass('noscroll');
+//   $( '#imagelightbox-overlay' ).fadeOut();
+// })
